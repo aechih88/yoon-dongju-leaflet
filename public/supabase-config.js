29d728a61c3449836f22a8a82ce895006169e9c6
aechih88 <aechi.h88@gmail.com>
2026-09-19 15:48:53 +0900

    Add public Supabase config

---
+++ public/supabase-config.js
@@ -0,0 +1,8 @@
+window.YOON_SUPABASE = {
+  url: 'https://snucytehbskfxoyocuzc.supabase.co',
+  anonKey: 'sb_publishable_Vp5AT--zD05Kt412cDEEpw_BjgowPqn',
+  table: 'yoon_leaflet_content',
+  rowId: 'main',
+  bucket: 'yoon-leaflet-assets',
+  adminEmail: 'hj120505@jface.or.kr'
+};
